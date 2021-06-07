0b8b37fac28c4a56dae9175bd14de0badd1294ee
samuel.fumero <samuelfumerohdez@gmail.com>
2021-06-07 14:46:32 +0100

    Primer commit - Configurando H2

---
+++ src/main/java/es/com/bitbox/bitbox2/Bitbox2Application.java
@@ -0,0 +1,16 @@
+package es.com.bitbox.bitbox2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+public class Bitbox2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bitbox2Application.class, args);
+	}
+
+}
---
+++ src/main/java/es/com/bitbox/bitbox2/web/Controlador.java
@@ -0,0 +1,31 @@
+package es.com.bitbox.bitbox2.web;
+
+import es.com.bitbox.bitbox2.Persona;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
+
+import static jdk.nashorn.internal.runtime.regexp.joni.Config.log;
+
+
+@Controller
+@Entity
+@Table
+public class Controlador {
+
+    @GetMapping("/")
+    public String inicio(Model model){
+
+        //model.addAttribute("personas", personas);
+        return "index";
+    }
+
+}
