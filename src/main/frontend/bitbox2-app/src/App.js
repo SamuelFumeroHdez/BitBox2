5d7b05bfd7f684e4538183a6102827cf1b25e31d
samuel.fumero <samuelfumerohdez@gmail.com>
2021-06-10 16:03:54 +0100

    Agregando al add de git los ficheros js

---
+++ src/main/frontend/bitbox2-app/src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { UserService } from './services/UserService';
+import {DataTable} from 'primereact/datatable';
+import {Column} from 'primereact/column';
+import { Panel } from 'primereact/panel';
+
+import 'primereact/resources/themes/nova-accent/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+
+export default class App extends Component{
+  constructor(){
+    super();
+    /*this.setState({
+      users: []
+    });*/
+    this.state = {};
+    this.userService = new UserService();
+  }
+  componentDidMount(){
+    this.userService.getAll().then(data => this.setState({users: data}))
+  }
+
+  render(){
+    return(
+      <Panel header="Usuarios" style={{width: '80%', marginTop: '20px', margin: '0 auto'}}>
+        <DataTable value={this.state.users}>
+          <Column field="iduser" header="ID"></Column>
+          <Column field="name" header="NAME"></Column>
+          <Column field="country" header="COUNTRY"></Column>
+        </DataTable>
+      </Panel>
+      
+    )
+  }
+
+}
