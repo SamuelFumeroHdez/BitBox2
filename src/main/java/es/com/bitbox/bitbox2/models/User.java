df69d6010f7f93b72a6d7250360efa057866a425
samuel.fumero <samuelfumerohdez@gmail.com>
2021-06-11 07:46:10 +0100

    Agregando modelo User al repositorio

---
+++ src/main/java/es/com/bitbox/bitbox2/models/User.java
@@ -0,0 +1,25 @@
+package es.com.bitbox.bitbox2.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long iduser;
+    private String name;
+    private String country;
+}
